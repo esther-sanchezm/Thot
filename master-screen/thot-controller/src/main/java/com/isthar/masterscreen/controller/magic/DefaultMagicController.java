709acc5350df6434dac76044e5b7c8540df913a3
esamarti <esamarti@everis.com>
2021-08-14 18:13:25 +0200

    Change structure

---
+++ master-screen/thot-controller/src/main/java/com/isthar/masterscreen/controller/magic/DefaultMagicController.java
@@ -0,0 +1,28 @@
+package com.isthar.masterscreen.controller.magic;
+
+import com.isthar.masterscreen.controller.magic.resource.Magic;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class DefaultMagicController implements MagicController {
+
+
+    @Override
+    public ResponseEntity<List<Magic>> findAll() {
+   /*     List<MagicDto> listDto = magicService.findAll();
+        List<Magic> listMagicResource = new ArrayList<>();
+       listDto.forEach((final MagicDto magicDto) ->
+                listMagicResource.add(Magic.builder()
+                        .name(magicDto.getName())
+                        .description(magicDto.getDescription()).build()));
+        return new ResponseEntity<>(listMagicResource,(listMagicResource.isEmpty() ? HttpStatus.OK : HttpStatus.NO_CONTENT));
+    */
+        return null;
+    }
+
+}
---
+++ master-screen/thot-controller/src/main/java/com/isthar/masterscreen/controller/race/resource/RaceResource.java
@@ -0,0 +1,4 @@
+package com.isthar.masterscreen.controller.race.resource;
+
+public class RaceResource {
+}
