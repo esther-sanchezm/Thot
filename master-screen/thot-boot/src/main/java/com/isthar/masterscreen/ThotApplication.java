b063dec706c2b20b681896296702825dd2d67ac5
esamarti <esamarti@everis.com>
2021-08-14 18:20:38 +0200

    Change name and port

--- master-screen/thot-boot/src/main/java/com/isthar/masterscreen/ThotApplication.java
+++ master-screen/thot-boot/src/main/java/com/isthar/masterscreen/ThotApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MasterScreenApplication {
+public class ThotApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MasterScreenApplication.class, args);
+		SpringApplication.run(ThotApplication.class, args);
 	}
 
 }
