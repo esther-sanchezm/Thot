7edb26dd4d3309a45bd5950f4dae24c3acf89a39
esamarti <esamarti@everis.com>
2021-08-15 01:30:32 +0200

    Move package to backend

--- master-screen/thot-controller/src/main/java/com/isthar/masterscreen/controller/character/resource/profile/CharacterProfileResource.java
+++
@@ -1,9 +0,0 @@
-package com.isthar.masterscreen.controller.character.resource.profile;
-
-public class CharacterProfileResource {
-
-    private String name;
-
-    private String photo;
-
-}
--- master-screen/thot-repository/src/main/java/com/isthar/masterscreen/repository/race/RaceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.isthar.masterscreen.repository.race;
-
-import com.isthar.masterscreen.repository.character.entity.CharacterDocument;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RaceRepository extends MongoRepository<CharacterDocument, Integer> {
-}
