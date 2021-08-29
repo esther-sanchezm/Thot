d44de8c10a459b0263c9cce860a27ec2f54bb859
esamarti <esamarti@everis.com>
2021-08-29 21:19:52 +0200

    Cleaning some dependencies

--- backend/thot-boot/src/main/java/com/isthar/masterscreen/ThotApplication.java
+++ backend/thot-boot/src/main/java/com/isthar/masterscreen/ThotApplication.java
@@ -2,10 +2,9 @@ package com.isthar.masterscreen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class ThotApplication {
 
 	public static void main(String[] args) {
