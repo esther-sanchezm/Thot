ce4792c16594cafb4f771de6d917f52567e79996
esamarti <esamarti@everis.com>
2021-09-01 22:39:37 +0200

    Complete spring fox

--- backend/thot-boot/src/main/java/com/thot/boot/config/log/LogAspect.java
+++ backend/thot-boot/src/main/java/com/thot/boot/config/log/LogAspect.java
@@ -13,11 +13,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class LogAspect {
 
-    /** A logger reference */
-    private static Logger logger = LoggerFactory.getLogger(LogAspect.class);
-
     /**
-     * Pointcut that matches in project com.everis.
+     * Pointcut that matches in project com.thot
      */
     @Pointcut("execution(public * com.thot..*(..))")
     public void applicantionBeanPointcut() {
@@ -33,11 +30,10 @@ public class LogAspect {
 
     }
 
-
     @Before("applicantionBeanPointcut() && springBeanPointcut()")
     public void logBefore(JoinPoint joinPoint) {
 
-        logger.info("Entering class: {}, executing method: {} with args: {}",
+        log.info("Entering class: {}, executing method: {} with args: {}",
                 joinPoint.getTarget().getClass().getName(),
                 joinPoint.getSignature().getName(),
                 ((joinPoint.getArgs() != null && joinPoint.getArgs().length > 0) ? joinPoint.getArgs()[0] : ""));
@@ -45,12 +41,12 @@ public class LogAspect {
 
     @AfterReturning(pointcut = "applicantionBeanPointcut() && springBeanPointcut()", returning = "result")
     public void logAfterReturning(JoinPoint joinPoint, Object result) {
-        logger.info("In class: {}, exiting method: {} with returned value: {}", joinPoint.getTarget().getClass().getName(),  joinPoint.getSignature().getName(), result);
+        log.info("In class: {}, exiting method: {} with returned value: {}", joinPoint.getTarget().getClass().getName(),  joinPoint.getSignature().getName(), result);
     }
 
     @AfterThrowing(pointcut = "springBeanPointcut()", throwing = "exception")
     public void logAfterThrowing(JoinPoint joinPoint, Throwable exception) {
-        logger.error("In class: {}, exception on method: {} exception is: {}", joinPoint.getTarget().getClass().getName(), joinPoint.getSignature().getName(), exception.getMessage());
+        log.error("In class: {}, exception on method: {} exception is: {}", joinPoint.getTarget().getClass().getName(), joinPoint.getSignature().getName(), exception.getMessage());
     }
 }
 
